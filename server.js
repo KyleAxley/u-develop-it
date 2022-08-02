83c28f956afcb4812d401a0c41bdfc99661465e3
KyleAxley <kanderson7645@yahoo.com>
2022-08-02 02:06:26 -0400

    query method added to return data

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+//import mysql2
+const mysql = require('mysql2');
 //import express
 const express = require('express');
 //port designation
@@ -8,9 +10,24 @@ const app = express();
 app.use(express.urlencoded({ express: false }));
 app.use(express.json()); 
 
+//Connect to database 
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //Your MySql username 
+        user: 'root',
+        // Your MySql password 
+        password: 'root',
+        database: 'election'
+    }, 
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 //Default response for any other request (Not Found)
-
 app.use((req, res) => {
     res.status(404).end(); 
 }); 
