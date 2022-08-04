d9ddaa22592b9f09b52a38d2a6703c189d2fbd99
KyleAxley <kanderson7645@yahoo.com>
2022-08-03 23:14:32 -0400

    JOIN query added/corresonding routes

--- server.js
+++ server.js
@@ -27,7 +27,11 @@ const db = mysql.createConnection(
 
 //Get all candidates
 app.get("/api/candidates", (req, res) => {
-  const sql = `SELECT * FROM candidates`;
+  const sql = `SELECT candidates.*, parties.name 
+  AS party_name 
+  FROM candidates 
+  LEFT JOIN parties 
+  ON candidates.party_id = parties.id`;
 
   db.query(sql, (err, rows) => {
     if (err) {
@@ -43,7 +47,13 @@ app.get("/api/candidates", (req, res) => {
 
 // Get a single candidate
 app.get("/api/candidate/:id", (req, res) => {
-  const sql = `SELECT * FROM candidates WHERE id = ?`;
+  const sql = `SELECT candidates.*, parties.name
+  AS party_name
+  FROM candidates
+  LEFT JOIN parties
+  ON candidates.parties_id = parties.id
+  WHERE candidates.id = ?`;
+  
   const params = [req.params.id];
 
   db.query(sql, params, (err, row) => {
@@ -104,18 +114,16 @@ app.post("/api/candidate", ({ body }, res) => {
     }
     res.json({
       message: "success",
-      data: body
+      data: body,
     });
   });
 });
 
-
 //Default response for any other request (Not Found)
 app.use((req, res) => {
-    res.status(404).end();
+  res.status(404).end();
 });
 
 app.listen(PORT, () => {
   console.log(`server is running on ${PORT}!`);
 });
-
